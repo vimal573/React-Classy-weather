2746da0419f7debe71e135c26a61cafed92331a0
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-15 10:01:08 +0530

    Starting the Classy weather App

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: 'lisbon',
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather() {
+    console.log('Loading Data...');
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search from location..."
+            value={this.state.location}
+            onChange={e => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get weather</button>
+      </div>
+    );
+  }
+}
+
+export default App;
